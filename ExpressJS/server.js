f0335e732af5382b9972a3f3b86e2260d6585ca3
Nishchit14 <dnishchit@gmail.com>
2018-04-23 18:13:42 +0530

    Initial Commit Faye Demo

---
+++ ExpressJS/server.js
@@ -0,0 +1,32 @@
+
+var http       = require('http'),
+    express    = require('express'),
+    bodyParser = require ('body-parser'),
+    morgan     = require ('morgan'),
+    faye       = require('faye');
+
+
+var bayeux = new faye.NodeAdapter({
+    mount:    '/faye',
+    timeout:  45
+});
+
+
+var app = express();
+var server = http.createServer(app);
+
+bayeux.attach(server);
+
+app.use(morgan());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+
+app.post('/message', function(req, res) {
+    bayeux.getClient().publish('/channel', {text: req.body.message});
+    res.sendStatus(200);
+});
+
+var port = process.env.port || 8000;
+server.listen(port);
+console.log('Server up and listening on port ' + port);
+
