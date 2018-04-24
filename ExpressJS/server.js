68a9d40f6aa6a96c13196bc8601b5c028baee27c
Nishchit14 <dnishchit@gmail.com>
2018-04-24 15:57:09 +0530

    Code refectoring

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -1,24 +1,27 @@
 const  express = require('express'),
        app = express()
 
-var http = require('http'),
-    faye = require('faye');
+const http = require('http'),
+      faye = require('faye');
 
-var server = http.createServer(app),
+const server = http.createServer(app),
         
-    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+      bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
 app.use(express.static(__dirname + '/public'))
 
-var client = new faye.Client('http://localhost:8000/faye')
+const client = new faye.Client('http://localhost:8000/faye')
+
+const newmessage = (newMessage) => {
+    console.log("New Message: ", newMessage)
+}
 
 /**
  * client to SUBSCRIBE (listen in server)
  * messages coming into the same CHANNEL (/messages)
  */
-client.subscribe('/messages', function (newMessage) {
-  console.log("New Message: ", newMessage)
- });
+client.subscribe('/messages', newmessage);
 
 bayeux.attach(server);
+
 server.listen(8000);
