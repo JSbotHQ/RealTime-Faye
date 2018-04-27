fea4905dafea6ccea68c916428c7e622eef574ea
Nishchit14 <dnishchit@gmail.com>
2018-04-27 14:48:11 +0530

    Code refectoring

--- HapiJS/server.js
+++ HapiJS/server.js
@@ -3,12 +3,12 @@ const Hapi = require('hapi');
 
 // Create a server with a host and port
 
-const server=Hapi.server({
-    host:'localhost',
-    port:8000
+const server = Hapi.server({
+    host: 'localhost',
+    port: 8000
 });
 
-const start = async ()=> {
+const start = async () => {
 
     try {
         await server.register(require('inert'));
@@ -24,11 +24,9 @@ const start = async ()=> {
 };
 start();
 
-
-
 // Faye
 const faye = require('faye');
-const bayeux = new faye.NodeAdapter({mount: '/faye',timeout: 45});
+const bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 const client = new faye.Client('http://localhost:8000/faye')
 // Faye via Hapi
 bayeux.attach(server.listener);
@@ -43,36 +41,32 @@ client.subscribe(`/message-serve`, newmessage);
 
 let connected_clients = []
 
-bayeux.on(`handshake`,(clientId)=> {
+bayeux.on(`handshake`, (clientId) => {
     connected_clients.push(clientId)
-    console.log(`connected`,connected_clients)
-    client.publish(`/onlineUsers`,{text: connected_clients})
+    console.log(`connected`, connected_clients)
+    client.publish(`/onlineUsers`, {text: connected_clients})
 })
 
-bayeux.on('disconnect',(clientId)=> {
+bayeux.on('disconnect', (clientId) => {
     let index = connected_clients.indexOf(clientId);
     if (index > -1) {
         connected_clients.splice(index, 1);
     }
-    console.log(`disconnected`,clientId)
+    console.log(`disconnected`, clientId)
 })
 
 
 // Serve static file
 // ROUTES
 server.route({
-    method:'GET',
-    path:'/chat',
-    handler: (request, h) => {
-        return h.file('./public/chat.html')
-    }
+    method: 'GET',
+    path: '/chat',
+    handler: (request, h) => h.file('./public/chat.html')
 });
 server.route({
-    method:'GET',
-    path:'/group',
-    handler:function(request, h) {
-        return h.file('./public/group.html')
-    }
+    method: 'GET',
+    path: '/group',
+    handler: (request, h) => h.file('./public/group.html')
 });
 
 
